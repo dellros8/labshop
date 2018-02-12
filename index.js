1276dbd5c04ed0443faaec2a5e2863570f366b97
Simon Dellros <simondellros@Simons-MacBook-Air-2.local>
2018-02-12 14:33:13 +0100

    gjorde lite på laben

---
+++ index.js
@@ -0,0 +1,43 @@
+let objectsArray = [apple, pear, orange, lime, melon, grapes];
+
+let apple = {
+    productname:"Apple",
+    price:"400",
+    description:"This is an apple",
+    image:"images/apple.png",
+};
+
+let pear = {
+    productname:"Pear",
+    price:"600",
+    description:"This is a pear",
+    image:"images/pear.jpg",
+};
+
+let orange = {
+    productname:"Orange",
+    price:"500",
+    description:"This is an orange",
+    image:"images/apple.jpg",
+};
+
+let lime = {
+    productname:"Lime",
+    price:"800",
+    description:"This is a lime",
+    image:"images/lime.jpg",
+};
+
+let melon = {
+    productname:"Melon",
+    price:"500",
+    description:"This is a melon",
+    image:"images/melon.jpg",
+};
+
+let grapes = {
+    productname:"Grapes",
+    price:"1000",
+    description:"This is grapes",
+    image:"images/grapes.jpg",
+};
